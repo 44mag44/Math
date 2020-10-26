4355b67396e001c3f181071c482e899bdc96421a
44mag44 <antistop91@mail.ru>
2020-10-26 22:07:37 +0900

    rm -> menu-1.2

--- geometry/foursquare.c
+++ geometry/foursquare.c
@@ -15,38 +15,17 @@ int side_area()
 		return 0;
 	}
 
-	S=a*a; // формула
+	S=a*a; // формула 
 
-	printf("S=%0.3f", S);
+	printf("S=%0.3f\n", S);
 	
 	return S;
 }
 
-// поиск площади по его диоганали
-int diagonal_area() 
-{
-	float S,d;
-
-	printf("d: ");
-	scanf("%f", &d);
-
-	if(d<1)
-	{
-		printf("\nпо мне так нету смысла!\n");
-		return 0;
-	}
-
-	S=(d*d)*0.5; //  1/2=0.5
-
-	printf("S=%0.3f", S);
-	
-	return S;
-}
-
+// квадрат
 int foursquare()
 {
 	printf("\t1. Поиск площади по его сторонам.\n");
-	printf("\t2. Поиск площади по его диоганали.\n");
 
 	int num;
 
@@ -57,9 +36,6 @@ int foursquare()
 	{
 	case 1:
 		side_area(); // поиск площади по сторонам квадрата
-		break;
-	case 2:
-		diagonal_area(); // поиск площади по диоганали кводрата
 		break;
 	default:
 		printf("Что то пошло не так =(\n");
